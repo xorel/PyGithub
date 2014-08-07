ea11617726572147746eed0bfaf58a4a672427a8
Vincent Jacques <vincent@vincent-jacques.net>
2014-08-06 19:00:50 -0700

    Use new test resources for Label and Milestone TestCases

--- PyGithub/Blocking/tests/classes/LabelTestCases.py
+++ PyGithub/Blocking/tests/classes/LabelTestCases.py
@@ -6,26 +6,26 @@ from PyGithub.Blocking.tests.Framework import *
 
 
 class LabelAttributes(TestCase):
-    @Enterprise.User(1)
+    @Enterprise("electra")
     def test(self):
-        l = self.g.get_repo(("ghe-user-1", "repo-user-1-1")).get_label("bug")
+        l = self.g.get_repo(("electra", "mutable")).get_label("bug")
         self.assertEqual(l.color, "fc2929")
         self.assertEqual(l.name, "bug")
 
 
 class LabelEdit(TestCase):
-    @Enterprise.User(1)
+    @Enterprise("electra")
     def testName(self):
-        l = self.g.get_repo(("ghe-user-1", "repo-user-1-1")).get_label("bug")
+        l = self.g.get_repo(("electra", "mutable")).get_label("bug")
         self.assertEqual(l.name, "bug")
         l.edit(name="feature")
         self.assertEqual(l.name, "feature")
         l.edit(name="bug")
         self.assertEqual(l.name, "bug")
 
-    @Enterprise.User(1)
+    @Enterprise("electra")
     def testColor(self):
-        l = self.g.get_repo(("ghe-user-1", "repo-user-1-1")).get_label("bug")
+        l = self.g.get_repo(("electra", "mutable")).get_label("bug")
         self.assertEqual(l.color, "fc2929")
         l.edit(color="aabbcc")
         self.assertEqual(l.color, "aabbcc")
@@ -34,8 +34,8 @@ class LabelEdit(TestCase):
 
 
 class LabelDelete(TestCase):
-    @Enterprise.User(1)
+    @Enterprise("electra")
     def test(self):
-        l = self.g.get_repo(("ghe-user-1", "repo-user-1-1")).create_label("to_be_deleted", "FF0000")
+        l = self.g.get_repo(("electra", "mutable")).create_label("ephemeral", "FF0000")
         self.assertEqual(l.color, "FF0000")
         l.delete()
--- PyGithub/Blocking/tests/classes/MilestoneTestCases.py
+++ PyGithub/Blocking/tests/classes/MilestoneTestCases.py
@@ -6,73 +6,72 @@ from PyGithub.Blocking.tests.Framework import *
 
 
 class MilestoneAttributes(TestCase):
-    @Enterprise.User(1)
+    @Enterprise("electra")
     def test(self):
-        m = self.g.get_repo(("ghe-user-1", "repo-user-1-1")).get_milestone(1)
-        self.assertEqual(m.closed_issues, 0)
-        self.assertEqual(m.created_at, datetime.datetime(2014, 7, 20, 6, 43, 19))
-        self.assertEqual(m.creator.login, "ghe-user-1")
-        self.assertEqual(m.description, "Body of first milestone")
-        self.assertEqual(m.due_on, datetime.datetime(2014, 7, 26, 0, 0))
-        self.assertEqual(m.id, 1)
-        self.assertEqual(m.labels_url, "http://github.home.jacquev6.net/api/v3/repos/ghe-user-1/repo-user-1-1/milestones/1/labels")
+        m = self.g.get_repo(("electra", "issues")).get_milestone(1)
+        self.assertEqual(m.closed_issues, 2)
+        self.assertEqual(m.created_at, datetime.datetime(2014, 8, 4, 3, 9, 47))
+        self.assertEqual(m.creator.login, "electra")
+        self.assertEqual(m.description, None)
+        self.assertEqual(m.due_on, None)
+        self.assertEqual(m.id, 17)
+        self.assertEqual(m.labels_url, "http://github.home.jacquev6.net/api/v3/repos/electra/issues/milestones/1/labels")
         self.assertEqual(m.number, 1)
-        self.assertEqual(m.open_issues, 3)
+        self.assertEqual(m.open_issues, 0)
         self.assertEqual(m.state, "open")
-        self.assertEqual(m.title, "First milestone")
-        self.assertEqual(m.updated_at, datetime.datetime(2014, 7, 23, 4, 47, 58))
-        self.assertEqual(m.url, "http://github.home.jacquev6.net/api/v3/repos/ghe-user-1/repo-user-1-1/milestones/1")
+        self.assertEqual(m.title, "Immutable milestone")
+        self.assertEqual(m.updated_at, datetime.datetime(2014, 8, 4, 3, 9, 49))
+        self.assertEqual(m.url, "http://github.home.jacquev6.net/api/v3/repos/electra/issues/milestones/1")
 
 
 class MilestoneEdit(TestCase):
-    @Enterprise.User(1)
+    @Enterprise("electra")
     def testTitle(self):
-        m = self.g.get_repo(("ghe-user-1", "repo-user-1-1")).get_milestone(1)
-        self.assertEqual(m.title, "First milestone")
-        m.edit(title="First milestone!")
-        self.assertEqual(m.title, "First milestone!")
-        m.edit(title="First milestone")
-        self.assertEqual(m.title, "First milestone")
+        m = self.g.get_repo(("electra", "issues")).get_milestone(3)
+        self.assertEqual(m.title, "Mutable milestone")
+        m.edit(title="Mutable milestone!")
+        self.assertEqual(m.title, "Mutable milestone!")
+        m.edit(title="Mutable milestone")
+        self.assertEqual(m.title, "Mutable milestone")
 
-    @Enterprise.User(1)
+    @Enterprise("electra")
     def testState(self):
-        m = self.g.get_repo(("ghe-user-1", "repo-user-1-1")).get_milestone(1)
+        m = self.g.get_repo(("electra", "issues")).get_milestone(3)
         self.assertEqual(m.state, "open")
         m.edit(state="closed")
         self.assertEqual(m.state, "closed")
         m.edit(state="open")
         self.assertEqual(m.state, "open")
 
-    @Enterprise.User(1)
+    @Enterprise("electra")
     def testDescription(self):
-        m = self.g.get_repo(("ghe-user-1", "repo-user-1-1")).get_milestone(1)
-        self.assertEqual(m.description, "Body of first milestone")
-        m.edit(description=PyGithub.Blocking.Reset)
-        self.assertIsNone(m.description)
+        m = self.g.get_repo(("electra", "issues")).get_milestone(3)
+        self.assertEqual(m.description, None)
         m.edit(description="Body of first milestone")
         self.assertEqual(m.description, "Body of first milestone")
+        m.edit(description=PyGithub.Blocking.Reset)
+        self.assertEqual(m.description, None)
 
-    @Enterprise.User(1)
+    @Enterprise("electra")
     def testDueOn(self):
-        m = self.g.get_repo(("ghe-user-1", "repo-user-1-1")).get_milestone(1)
-        self.assertEqual(m.due_on, datetime.datetime(2014, 7, 26, 0, 0))
-        m.edit(due_on=PyGithub.Blocking.Reset)
-        self.assertIsNone(m.due_on)
+        m = self.g.get_repo(("electra", "issues")).get_milestone(3)
+        self.assertEqual(m.due_on, None)
         m.edit(due_on="2014-07-26T00:00:00Z")
         self.assertEqual(m.due_on, datetime.datetime(2014, 7, 26, 0, 0))
+        m.edit(due_on=PyGithub.Blocking.Reset)
+        self.assertEqual(m.due_on, None)
 
 
 class MilestoneLabels(TestCase):
-    @Enterprise.User(1)
+    @Enterprise("electra")
     def testGetLabels(self):
-        m = self.g.get_repo(("ghe-user-1", "repo-user-1-1")).get_milestone(1)
+        m = self.g.get_repo(("electra", "issues")).get_milestone(1)
         labels = m.get_labels()
         self.assertEqual([l.name for l in labels], ["enhancement", "question"])
 
 
 class MilestoneDelete(TestCase):
-    @Enterprise.User(1)
+    @Enterprise("electra")
     def test(self):
-        m = self.g.get_repo(("ghe-user-1", "repo-user-1-1")).create_milestone("Created by PyGithub")
-        self.assertIsNone(m.description)
+        m = self.g.get_repo(("electra", "issues")).create_milestone("Created by PyGithub")
         m.delete()
