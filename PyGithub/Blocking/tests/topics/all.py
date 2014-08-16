b2d19760c428a1bbe85ff4876ee98575fd72096b
Vincent Jacques <vincent@vincent-jacques.net>
2014-08-16 06:43:04 -0700

    Remove useless test cases

--- PyGithub/Blocking/tests/topics/all.py
+++ PyGithub/Blocking/tests/topics/all.py
@@ -3,6 +3,5 @@
 # Copyright 2013-2014 Vincent Jacques <vincent@vincent-jacques.net>
 
 from PyGithub.Blocking.tests.topics.AuthenticationTestCases import *
-from PyGithub.Blocking.tests.topics.DebugMessagesTestCases import *
 from PyGithub.Blocking.tests.topics.PaginationTestCases import *
 from PyGithub.Blocking.tests.topics.UnusualErrorsTestCases import *
