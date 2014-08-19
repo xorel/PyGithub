f71901fbf7edd6ac441621283319f96d5df290e8
Vincent Jacques <vincent@vincent-jacques.net>
2014-08-18 18:41:59 -0700

    Check main code for asserts (follow-up of #268)

--- manage/check.py
+++ manage/check.py
@@ -7,16 +7,14 @@ import subprocess
 
 
 def main():
-    subprocess.check_call([
-        "pep8", "--ignore=E501", ".",
-    ])
+    subprocess.check_call('pep8 --ignore=E501 .', shell=True)
 
-    for line in subprocess.check_output([
-        "git", "grep", "-n", "^ *class ", "--", "PyGithub/**.py"
-
-    ], universal_newlines=True).split("\n"):
-        if "(" not in line and line != "":
+    for line in subprocess.check_output('grep -n "^ *class" $(find PyGithub -name "*.py")', universal_newlines=True, shell=True).split("\n")[:-1]:
+        if "(" not in line:
             print("Should this class inherit from object?", line)
+
+    for line in subprocess.Popen('grep -n "assert " PyGithub/Blocking/*.py', universal_newlines=True, shell=True, stdout=subprocess.PIPE).communicate()[0].split("\n")[:-1]:
+        print("Seriously, an assert in the main code?", line)
 
 if __name__ == "__main__":
     main()
